827243866f8de4354cb6bfa4e48d38f5339fd280
Michal Piekos <michal.piekos@wp.pl>
2025-10-05 08:00:01 +0200

    feat(template): text mode friendly status line
    Signed-off-by: Michal Piekos <michal.piekos@wp.pl>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,10 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
+				theme = "gruvbox",
+                                icons_enabled = false,
+                                component_separators = {left = '|', right = '|'},
+                                section_separators = {left = '', right = ''},
 			},
 			sections = {
 				lualine_z = {                                                                                                                                           
